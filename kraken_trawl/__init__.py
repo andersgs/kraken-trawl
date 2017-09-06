b9866d3089b332e7d024848acddbb398331ec396
Anders Goncalves da Silva <andersgs@gmail.com>
2017-09-07 08:13:04 +1000

    Updated __init__.py to import functions.

--- kraken_trawl/__init__.py
+++ kraken_trawl/__init__.py
@@ -1,8 +1,9 @@
-__name__            = 'kraken_trawl'
-__version__         = '0.0.1'
-__description__     = 'Create custom kraken DB'
-__author__          = 'Anders Goncalves da Silva'
-__author_email__    = 'andersgs@gmail.com'
-__url__             = 'https://github.com/andersgs/kraken-trawl'
-__license__         = 'GPLv3'
-__all__             = ["kraken-trawl"]
+from kraken_trawl import kraken_trawl
+__name__ = 'kraken_trawl'
+__version__ = '0.0.2'
+__description__ = 'Create custom kraken DB'
+__author__ = 'Anders Goncalves da Silva'
+__author_email__ = 'andersgs@gmail.com'
+__url__ = 'https://github.com/andersgs/kraken-trawl'
+__license__ = 'GPLv3'
+__all__ = ["kraken-trawl"]
